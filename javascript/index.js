eb93a5baaeb38dfb68a62a3b19339aeb61252d62
amitsa1234 <137153069+amitsa1234@users.noreply.github.com>
2023-06-24 14:57:14 +0300

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,19 +1,19 @@
-var maxAttempts = 3;
-var successfulLogin = false;
+// var maxAttempts = 3;
+// var successfulLogin = false;
 
-for (var attempts = 1; attempts <= maxAttempts; attempts++) {
-  var username = prompt('נא הזן את שם המשתמש:');
-  var password = prompt('נא הזן את הקוד:');
+// for (var attempts = 1; attempts <= maxAttempts; attempts++) {
+//   var username = prompt('נא הזן את שם המשתמש:');
+//   var password = prompt('נא הזן את הקוד:');
 
-  if (username === 'admin' && password === '12345') {
-    successfulLogin = true;
-    alert('פרטי הכניסה נכונים! מיד תועבר.');
-    break;
-  } else {
-    alert('שם משתמש או קוד שגויים! נסה שוב.');
-  }
-}
+//   if (username === 'admin' && password === '12345') {
+//     successfulLogin = true;
+//     alert('פרטי הכניסה נכונים! מיד תועבר.');
+//     break;
+//   } else {
+//     alert('שם משתמש או קוד שגויים! נסה שוב.');
+//   }
+// }
 
-if (!successfulLogin) {
-  window.location.href = 'enter.html';
-}
+// if (!successfulLogin) {
+//   window.location.href = 'enter.html';
+// }
